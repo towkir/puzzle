550eeaac2e2565d3e4a18fa42ce545981e782c85
Towkir Ahmed <towkir17@gmail.com>
2018-10-22 20:51:58 +0600

    a little bit code cleaning and removed a few html and handled with js; more logics to solve

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 
+var puzzleSize = 25; // this is the slide number in the puzzle
+var board = document.getElementById('board');
+
+// variables for the style position property of each slides;
+var posTop = 0;
+var posLeft = 0;
+// and these for the background position of each slides;
+var bgPosX = 0;
+var bgPosY = 0;
+
+// now creating the slides and pushing them inside the container #board;
+for (var s = 1; s < puzzleSize + 1; s++ ) {
+    var slide = document.createElement('div');
+    var border = document.createElement('div');
+    if (s === 5){
+        border.style.border = 'none';
+    }
+    border.classList.add('border');
+    slide.classList.add('slide');
+    slide.dataset.originPosition = s;
+    slide.dataset.tempPosition = s;
+    slide.style.left = posLeft + '%';
+    slide.style.top = posTop + '%';
+    slide.style.backgroundPosition = bgPosX + '%' + bgPosY + '%';
+    slide.appendChild(border);
+    board.appendChild(slide);
+    posLeft += 20;
+    bgPosX -= 100;
+    if (s % 5 === 0){
+        posTop += 20;
+        posLeft = 0;
+        bgPosX = 0;
+        bgPosY -= 100;
+    }
+}
 
 //creating an array of the slides
-var slide = document.getElementsByClassName("box");
-var blank = slide[Math.sqrt(slide.length) - 1];  // to identify the blank slide 
-var root = Math.sqrt(slide.length);
-for (var i=0; i<slide.length; i++){
-  slide[i].frameNumber = i+1;
-  slide[i].coOrdinate = i+1;  // this changes with the slide position . it is never constant !!!
+var slideEl = document.getElementsByClassName("slide");
+var blank = slideEl[Math.sqrt(slideEl.length) - 1];  // to identify the blank slide
+blank.style.background = 'none';
+blank.style.boxShadow = 'none';
+blank.style.border = 'none';
+var root = Math.sqrt(slideEl.length);
+for (var i=0; i<slideEl.length; i++){
+  slideEl[i].frameNumber = i+1;
+  slideEl[i].coOrdinate = i+1;  // this changes with the slide position . it is never constant !!!
 }
 
 var slideTraced = function(number){
-  for (var a=0; a<slide.length; a++){
-    if (slide[a].coOrdinate == number){
-      return slide[a];
+  for (var a=0; a<slideEl.length; a++){
+    if (slideEl[a].coOrdinate == number){
+      return slideEl[a];
     }
   }
 };
@@ -22,7 +60,7 @@ var randomization = function(){
    var movementOrder = [1, root, -1, -root];
  for (var i=0; i<=100; i++){
    var guestPosition = blank.coOrdinate + (movementOrder[Math.floor(Math.random() * movementOrder.length)]);
-     if (guestPosition > 0 && guestPosition <= slide.length){
+     if (guestPosition > 0 && guestPosition <= slideEl.length){
        var guest = slideTraced(guestPosition);
    var left = blank.coOrdinate;
    var right = guest.coOrdinate;
@@ -70,8 +108,8 @@ var changePosition = function(){
 };
 
 //applies changePosition function on all of the slides...
-for (var i=0; i<slide.length; i++){
-  slide[i].addEventListener("click", changePosition);
+for (var i=0; i<slideEl.length; i++){
+  slideEl[i].addEventListener("click", changePosition);
 }
 
 
